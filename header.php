57be5b57b7071cd25f45681311dcff183eb81adf
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-08-02 09:45:09 -0300

    fica marcado na barra de navegação qual é a página que estamos

--- header.php
+++ header.php
@@ -9,10 +9,10 @@ $pagina_corrente = basename($_SERVER['SCRIPT_NAME']);
         <div class="nav-wrapper">
             <a href="#" class="brand-logo">Logo</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li>  <?php if ($pagina_corrente == 'index.php') {echo 'class = "active"';}?> <a href="index.php">Home</a></li>
-                <li><a href="destinos.php">Destinos</a></li>
-                <li><a href="quem.php">Quem somos</a></li>
-                <li><a href="login.php">Login</a></li>
+                <li <?php if ($pagina_corrente == 'index.php') {echo 'class = "active"';}?>> <a href="index.php">Home</a></li>
+                <li <?php if ($pagina_corrente == 'destinos.php') {echo 'class = "active"';}?>> <a href="destinos.php">Destinos</a></li>
+                <li <?php if ($pagina_corrente == 'quem.php') {echo 'class = "active"';}?>> <a href="quem.php">Quem somos</a></li>
+                <li <?php if ($pagina_corrente == 'login.php') {echo 'class = "active"';}?>> <a href="login.php">Login</a></li>
             </ul>
         </div>
     </nav>
