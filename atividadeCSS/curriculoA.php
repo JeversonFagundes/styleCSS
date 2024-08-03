5817af976faa0441abe23abca0eb5e9bd768c324
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-08-02 23:46:42 -0300

    comiti

---
+++ atividadeCSS/curriculoA.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="pt-br" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Curriculo</title>
+
+</head>
+
+<body>
+
+    <div class="container">
+        <h1>Fulano da Silva</h1>
+        <p class="p1">Nacionalidade brasileira, 20 anos, casado, natural da cidade de Uruguaiana.</p>
+
+
+        <h2>Experiência profissional</h2>
+
+        <h3><a href="http://uol.com.br" target="_blank">Farmácias Panvel</a> 2 meses</h3>
+
+        <h4 class="ul1">Analista Programador - 10 meses</h4>
+        <p>Desenvolvimento de aplicações web com HTML, CSS, Javascript e PHP.</p>
+
+        <h3><a href="http://terra.com.br" target="_blank"> Rede Globo</a> - 1 ano</h3>
+
+
+        <h4>Desenvolvedor Front End, 1 ano</h4>
+        <p>Desenvolvedor Front-End (aplicão WEB e mobile).</p>
+
+        <p>Utilização de tecnologias como HTML5, CSS, Wordpress, PHP e Javascript.</p>
+
+        <h3><a href="http://g1.com" target="_blank">Google</a> - 6 meses</h3>
+
+        <h4>Desenvolvedor web, 6 meses</h4>
+        <p> Integração de inteligencia artificial de recomendações, vitrine e funções para a loja em Ecommerce e desenvolvimento de aplicações.</p>
+
+        <div class="div">
+            <h2 class="h2">Formação</h2>
+            <h3>Graduacão em Tecnologia da Informação </h3>
+            <p class="destaque">01/2025 a 12/2027</p>
+
+            <p>FIAP - Faculdade de Tecnologia e Administração Paulista </p>
+
+            <h3>Pós-graduação em sistemas inteligentes</h3>
+            <p class="destaque">08/2028 a 12/2029</p>
+
+            <p>UFRGS</p>
+        </div>
+
+        <h2>Cursos / Especializações</h2>
+
+        <h3>Programação Swift 2</h3>
+        <p>Máxima informática 90h, 2028</p>
+
+        <h3>Bootstrap</h3>
+        <p>InfoVida 95h - 2025</p>
+
+        <h3>CSS3 e sites responsivos</h3>
+        <p>InfoVida 50h - 2026</p>
+
+        <div class="div2">
+            <h2 class="h22">Idiomas</h2>
+            <h3> Inglês </h3>
+            <p> Nível avançado</p>
+            <h3> Espanhol </h3>
+            <p> Nível intermediário</p>
+        </div>
+        <h2>Informações adicionais</h2>
+
+        <p> Possui um Endereço alternativo para trabalhos na cidade de São Paulo,
+            próximo a Estação Lago Azul.
+            Endereço: Estância Lago Negro - RS.
+            Idiomas</p>
+    </div>
+</body>
+
+</html>
