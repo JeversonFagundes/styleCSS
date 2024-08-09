e2cc93369ca7adf6bd5bd6bf249192075c4c183d
Jeverson Fagundes <jeverson.2022311922@aluno.iffar.edu.br>
2024-08-09 17:10:13 -0300

    dedededed

--- Materializer/header.php
+++ Materializer/header.php
@@ -6,15 +6,15 @@ $pagina_corrente = basename($_SERVER['SCRIPT_NAME']);
 <div class="navbar-fixed">
 
     <nav class="light-green lighten-3">
-        <div class="nav-wrapper">
-            <a href="#" class="brand-logo">Logo</a>
+        <div class="nav-wrapper container" >
+            <a href="#" class="brand-logo"><img src="sair.png" height="55" width="60"></a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li <?php if ($pagina_corrente == 'index.php') {echo 'class = "active"';}?>> <a href="index.php">Home</a></li>
-                <li <?php if ($pagina_corrente == 'destinos.php') {echo 'class = "active"';}?>> <a href="destinos.php">Destinos</a></li>
-                <li <?php if ($pagina_corrente == 'quem.php') {echo 'class = "active"';}?>> <a href="quem.php">Quem somos</a></li>
-                <li <?php if ($pagina_corrente == 'login.php') {echo 'class = "active"';}?>> <a href="login.php">Login</a></li>
+                <li <?php if ($pagina_corrente == 'index.php') {echo 'class = "active"';}?>> <a class="#00796b teal-text text-darken-2" href="index.php">Home</a></li>
+                <li <?php if ($pagina_corrente == 'destinos.php') {echo 'class = "active"';}?>> <a class="#00796b teal-text text-darken-2" href="destinos.php">Destinos</a></li>
+                <li <?php if ($pagina_corrente == 'quem.php') {echo 'class = "active"';}?>> <a class="#00796b teal-text text-darken-2" href="quem.php">Quem somos</a></li>
+                <li <?php if ($pagina_corrente == 'login.php') {echo 'class = "active"';}?>> <a class="#00796b teal-text text-darken-2" href="login.php">Login</a></li>
             </ul>
         </div>
     </nav>
-
+    #004d40 teal darken-4
 </div>
